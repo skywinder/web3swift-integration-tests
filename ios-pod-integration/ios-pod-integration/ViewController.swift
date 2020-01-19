62273a82d57006571690c2a8d740ca0f1e1282a7
Petr Korolev <sky4winder@gmail.com>
2020-01-19 14:59:05 +0700

    gen wallet test

--- ios-pod-integration/ios-pod-integration/ViewController.swift
+++ ios-pod-integration/ios-pod-integration/ViewController.swift
@@ -9,16 +9,46 @@
 import UIKit
 import web3swift
 
+struct Wallet {
+    let address: String
+    let data: Data
+    let name: String
+    let isHD: Bool
+}
+
+struct HDKey {
+    let name: String?
+    let address: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var seed_tv: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        gen_key(self)
         // Do any additional setup after loading the view.
         
     }
-
+    @IBOutlet weak var pub_label: UILabel!
+    
     @IBAction func gen_key(_ sender: Any) {
+        let password = "web3swift"
+        let bitsOfEntropy: Int = 128 // Entropy is a measure of password strength. Usually used 128 or 256 bits.
+        let mnemonics = try! BIP39.generateMnemonics(bitsOfEntropy: bitsOfEntropy)!
+        let keystore = try! BIP32Keystore(
+            mnemonics: mnemonics,
+            password: password,
+            mnemonicsPassword: "",
+            language: .english)!
+        let name = "New HD Wallet"
+        let keyData = try! JSONEncoder().encode(keystore.keystoreParams)
+        let address = keystore.addresses!.first!.address
+        let wallet = Wallet(address: address, data: keyData, name: name, isHD: true)
+        pub_label.text = address
+        pub_label.adjustsFontSizeToFitWidth = true
+        seed_tv.text = mnemonics
+        
     }
     
 }
