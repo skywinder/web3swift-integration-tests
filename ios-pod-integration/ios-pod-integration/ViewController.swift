44f6265da3c153facb7152db7549eb5ead303a40
Petr Korolev <sky4winder@gmail.com>
2020-01-19 14:01:34 +0700

    ios-pod

---
+++ ios-pod-integration/ios-pod-integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-pod-integration
+//
+//  Created by Petr Korolev on 19.01.2020.
+//  Copyright © 2020 Petr Korolev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
